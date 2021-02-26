937b89999b952c7b378a4e37247822d1d8373289
Daniel Tobias <dan.yb.tobias@gmail.com>
2021-02-26 00:47:55 -0800

    3d support

--- plot.py
+++ plot.py
@@ -2,10 +2,12 @@ from polya.distributions import Distribution
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
+from mpl_toolkits import mplot3d
+import numpy as np
 
-
-fig,ax = plt.subplots()
-dist = Distribution(2)
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+dist = Distribution(3)
     
 line, = ax.plot(0, 0, 'o')
 def init():
@@ -16,8 +18,15 @@ def animate(i):
     dist.update_pos()
     x = dist.pos[0]
     y = dist.pos[1]
-    line.set_data(x,y)
+    z = dist.pos[2]
+    line.set_data(np.asarray([x]),np.asarray([y]))
+    line.set_3d_properties(np.asarray([z]),'z')
     return line,
+
+ax.set_xlim3d([-5.0, 5.0])
+ax.set_ylim3d([-5.0, 5.0])
+ax.set_zlim3d([-5.0, 5.0])
+
 ax.axis([-5,5,-5,5])
 anim = FuncAnimation(fig, animate, init_func=init, frames=200, interval=1000, blit=True)
 FFMpegWriter = animation.writers['ffmpeg']
